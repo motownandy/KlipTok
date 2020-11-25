23764dc2179f90f6afb29eebe14651bc247689c0
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2020-11-25 11:28:31 -0500

    Adjusting player for screen size

--- KlipTok.Client/wwwroot/js/page.js
+++ KlipTok.Client/wwwroot/js/page.js
@@ -4,7 +4,12 @@
 
     setTitle : function(newTitle) {
       window.document.title = newTitle;
-    }
+    },
+    getWidth: function () {
+      return window.innerWidth
+        || document.documentElement.clientWidth
+        || document.body.clientWidth;
+		}
 
   };
 
