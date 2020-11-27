7fdb2540fba3e7e92c622913f626517d1b80fd65
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2020-11-27 15:01:16 -0500

    Ensuring a user is logged in before Liking a video

---
+++ KlipTok.Client/Formatters/TwitchUrl.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KlipTok.Client.Formatters
+{
+	public class TwitchUrl
+	{
+
+		public static Uri GetLoginUrl(IConfiguration config, NavigationManager nav) {
+			var clientId = config["twitch.clientid"];
+			var thisUri = new Uri(nav.BaseUri);
+			var port = thisUri.IsDefaultPort ? "" : (":" + thisUri.Port);
+			var redirect = new Uri(new Uri(thisUri.Scheme + "://" + thisUri.Host + port), "/authorize");
+			var scopes = "";
+			var url = new Uri($"https://id.twitch.tv/oauth2/authorize?client_id={clientId}&redirect_uri={redirect}&response_type=code&scope={scopes}");
+
+			return url;
+		}
+
+	}
+}
